0913e1be2fdb51988578561009388a8fbfa787b8
julymeltdown <quintuplets2000@gmail.com>
2023-07-07 18:25:51 +0900

    Error Detected

--- CsvValidator.kt
+++ CsvValidator.kt
@@ -1,3 +1,5 @@
+
+import com.gausslab.weatherapi.input.exception.FieldTypeNotSameWithColumnException
 import org.springframework.web.multipart.MultipartFile
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -10,19 +12,19 @@ object CsvValidator {
         val reader = BufferedReader(InputStreamReader(file.inputStream))
         // CSV 파일이 비어있는지 확인
         val header: Array<out String> = reader.readLine()?.split(",")?.toTypedArray()
-                ?: throw Exception("CSV file is empty")
+                ?: throw Exception()
         println(header)
         // Class의 모든 필드명 가져옴
         val fields = entityClass.kotlin.memberProperties.mapNotNull { it.javaField }.toList()
 
-        // csv 파일의 컬럼명이랑 entity 필드명 같은지다른지 확인
+        // 컬럼 개수 안맞음
         if (header.size != fields.size - ignoredFields.size) {
-            throw Exception("Number of columns in CSV file doesn't match number of fields in entity class")
+            throw Exception()
         }
-
+        // csv 파일의 컬럼명이랑 entity 필드명 같은지다른지 확인
         for (i in header.indices) {
             if (fields.find { it.name == header[i] } == null) {
-                throw Exception("CSV file's column names are not in the entity class's field names")
+                throw Exception()
             }
         }
 
@@ -31,10 +33,6 @@ object CsvValidator {
         while (reader.readLine().also { line = it?.split(",")?.toTypedArray() } != null) {
             val entityInstance = entityClass.getDeclaredConstructor().newInstance()
 
-            if (line!!.size != fields.size - ignoredFields.size) {
-                throw Exception("Number of values in a row doesn't match number of fields in entity class")
-            }
-
             for (i in line!!.indices) {
                 val fieldName = header[i]
                 val field: Field? = fields.find { it.name == fieldName }
@@ -42,24 +40,28 @@ object CsvValidator {
                 if (field == null || ignoredFields.contains(fieldName)) {
                     continue
                 }
-
                 field.trySetAccessible()
+                //여기가 고쳐져야하는 이유
+                // 지금 csv 파일의 각각의 값이 어떤 데이터 타입으로넘어올지가 정해지지 않음
+                // -> fdsafds이런걸 입력해도 일단은 DB에는 String으로 저장하고있어 정상 저장됨
+                // 파일의 자료형이 어떤 데이터 타입이 될지를 요청해서 알아야함...................
                 try {
                     val value = line!![i]
                     when (field.type) {
+
                         String::class.java -> field.set(entityInstance, value)
-                        Long::class.java -> field.set(entityInstance, value.toLong())
-                        Float::class.java -> field.set(entityInstance, value.toFloat())
-                        Double::class.java -> field.set(entityInstance, value.toDouble())
-                        Int::class.java -> field.set(entityInstance, value.toInt())
+                        Long::class.java -> field.set(entityInstance, value.toLongOrNull())
+                        Float::class.java -> field.set(entityInstance, value.toFloatOrNull())
+                        Double::class.java -> field.set(entityInstance, value.toDoubleOrNull())
+                        Int::class.java -> field.set(entityInstance, value.toIntOrNull())
                         Char::class.java -> field.set(entityInstance, value[0])
-                        Boolean::class.java -> field.set(entityInstance, value.toBoolean())
-                        Short::class.java -> field.set(entityInstance, value.toShort())
-                        Byte::class.java -> field.set(entityInstance, value.toByte())
-                        else -> throw Exception("CSV file value type doesn't match entity class's field type")
+                        Boolean::class.java -> field.set(entityInstance, value.toBooleanStrictOrNull())
+                        Short::class.java -> field.set(entityInstance, value.toShortOrNull())
+                        Byte::class.java -> field.set(entityInstance, value.toByteOrNull())
+                        else -> throw Exception()
                     }
-                } catch (e: NumberFormatException) {
-                    throw Exception("CSV file value type doesn't match entity class field type", e)
+                } catch (e: Exception) {
+                    throw FieldTypeNotSameWithColumnException()
                 }
             }
             resultEntityList.add(entityInstance)
